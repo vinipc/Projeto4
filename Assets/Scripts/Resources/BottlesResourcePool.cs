23e5da7222d68b32ecfd68c55e48769f5be58f4b
Vinícius Pimentel <vinipcouto@gmail.com>
2017-04-26 16:18:12 -0300

    Added a nice bottle pile for the analog activity display

---
+++ Assets/Scripts/Resources/BottlesResourcePool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottlesResourcePool : ResourcePool
+{
+	public GameObject bottlePrefab;
+	public Transform bottlesParent;
+	public float resourcesPerBottle = 10f;
+
+	private List<GameObject> bottles = new List<GameObject>();
+
+	public override void AddResource(float amountDelta)
+	{
+		base.AddResource(amountDelta);
+
+		if ((amount - resourcesPerBottle * bottles.Count) > resourcesPerBottle)
+		{
+			bottles.Add(Instantiate<GameObject>(bottlePrefab, bottlesParent));
+		}
+	}
+}
