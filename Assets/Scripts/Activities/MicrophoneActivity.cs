156495da105fdc9002e6912e13267999dd4f6016
Vinícius Pimentel <vinipcouto@gmail.com>
2017-04-27 15:06:38 -0300

    - Renamed GrapeCollectActivity to MicrophoneActivity - MicrophoneActivity resource generation is now proportional to volume.

--- Assets/Scripts/Activities/MicrophoneActivity.cs
+++ Assets/Scripts/Activities/MicrophoneActivity.cs
@@ -2,22 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrapeCollectActivity : Activity
+public class MicrophoneActivity : Activity
 {
 	private readonly int SAMPLE_COUNT = 1024; 
-	private readonly float THRESHOLD = 10f;
+	public readonly float MIC_SENSITIVITY = 100f; // Multiplies volume into more intelligible values
+
+	[Header("Read only:")]
+	public float volume; // Current volume
+	public float accumulatedVolume = 0f; // Current accumulated volume
+
+	[Header("Activity config:")]
+	public float threshold = 100f; // How much accumulated volume there must be to generate resource
+	public float ambientVolume = 0.6f; // Ambient volume to adjust input volume
+	public int generatedAmount; // How much resource is generated whenever threshold is beaten
 
 	private float[] _samples;
-	public float micSensitivity = 10f;
-
 	private AudioSource audioSource;
 
-	void Awake()
+	private void Awake()
 	{
 		audioSource = GetComponent<AudioSource>();
 	}
 
-	void Start() 
+	private void Start() 
 	{
 		// Intitialize the audio buffer
 		_samples = new float[SAMPLE_COUNT];
@@ -29,34 +36,41 @@ public class GrapeCollectActivity : Activity
 		SetupMicrophoneInput();
 	}
 
-	void Update()
+	private void Update()
 	{
 		if (GameMaster.isCounting)
 		{
-			float volume = GetAverageVolume() * micSensitivity;
-			if (volume > THRESHOLD)
-			{
-				ThresholdBeaten();
-			}			
+			CheckInput();
 		}
 	}
 
-	private void ThresholdBeaten()
+	private void CheckInput()
 	{
-		if (GameMaster.isCounting == true)
+		volume = GetAverageVolume() * MIC_SENSITIVITY;
+
+		// Factors in ambient volume without letting it go into negative
+		float adjustedVolume = volume - ambientVolume; 
+		adjustedVolume = Mathf.Max(0f, adjustedVolume);
+
+		accumulatedVolume += adjustedVolume;
+
+		if (accumulatedVolume >= threshold)
 		{
-			generatedResource.AddResource(1);
+			int timesThresholdBeaten = (int) (accumulatedVolume / threshold);
+			accumulatedVolume -= timesThresholdBeaten * threshold;
+
+			GenerateResource(generatedAmount * timesThresholdBeaten);
 		}
 	}
 
 	private float GetAverageVolume()
 	{
 		float average = 0.0f;
-		audioSource.GetOutputData (_samples, 0);
+		audioSource.GetOutputData(_samples, 0);
 
-		foreach(var sample in _samples) 
+		for (int i = 0; i < _samples.Length; i++)
 		{
-			average += Mathf.Abs(sample);
+			average += Mathf.Abs(_samples[i]);
 		}
 
 		return average / SAMPLE_COUNT;
@@ -75,13 +89,13 @@ public class GrapeCollectActivity : Activity
 		}
 		else
 		{
-			Debug.Log("Primarey recording device: " + primaryAudioRecordingDevice);
+			Debug.Log("Primary recording device: " + primaryAudioRecordingDevice);
 		}
 
 		// Do the microphone audio setup
 		AudioClip audioClip = Microphone.Start(primaryAudioRecordingDevice, true, 10, 44100);
 		audioSource.loop = true;
-		audioSource.mute =  false;
+		audioSource.mute = false;
 		audioSource.clip = audioClip;
 
 		// Gambeta
