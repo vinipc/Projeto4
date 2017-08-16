e09b2e2fa017adb6a0e31a6c666dbc0ea15ec960
Vinícius Pimentel <vinipcouto@gmail.com>
2017-08-16 17:58:19 -0300

    DanceMat is now slightly rythmic. Adjustments still need to be made

--- Assets/Scripts/Activities/DanceMatActivity.cs
+++ Assets/Scripts/Activities/DanceMatActivity.cs
@@ -5,18 +5,50 @@ using System;
 
 public class DanceMatActivity : Activity
 {
+	private float SPAWN_DISTANCE = 5f;
+
+	public Transform grapePrefab;
+	public Transform target;
+	public float hitRadius;
+
+	public float beatsInterval;
+	public float grapesSpeed;
+
 	[Header("Activity config:")]
 	public int resourcePerTap = 1;
 
+	private List<Transform> fallingGrapes = new List<Transform>();
 	private DanceMatInput lastPressedInput;
 	private Array danceMatInputsArray = System.Enum.GetValues(typeof(DanceMatInput));
+	private Countdown beatCountdown;
+
+	private void Start()
+	{
+		beatCountdown = Countdown.New(beatsInterval, SpawnGrape, UpdateGrapesPosition, true);
+	}
 
 	private void Update()
 	{
 		if (GameMaster.isCounting)
 		{
-			CheckInput();			
+			CheckInput();
 		}
+
+		beatCountdown.totalTime = beatsInterval;
+	}
+
+	private void UpdateGrapesPosition()
+	{		
+		for (int i = 0; i < fallingGrapes.Count; i++)
+		{
+			fallingGrapes[i].transform.position += Vector3.down * grapesSpeed * Time.deltaTime;
+		}
+	}
+
+	private void SpawnGrape()
+	{
+		Transform newGrape = Instantiate<Transform>(grapePrefab, target.position + Vector3.up * SPAWN_DISTANCE, Quaternion.identity, transform);
+		fallingGrapes.Add(newGrape);
 	}
 
 	private void CheckInput()
@@ -31,7 +63,20 @@ public class DanceMatActivity : Activity
 		// If the pressed button is different than the last pressed button, 
 		//	increases the score
 		if (pressedButton != lastPressedInput)
-			GenerateResource(resourcePerTap);
+		{
+			Transform closestGrape = GetClosestGrape();
+			if (Mathf.Abs(closestGrape.position.y - target.position.y) < hitRadius)
+			{
+				Debug.Log("Hit grape!");
+				fallingGrapes.Remove(closestGrape);
+				Destroy(closestGrape.gameObject);
+				GenerateResource(resourcePerTap);
+			}
+			else
+			{
+				Debug.Log("Missed grape :(");
+			}
+		}
 
 		// Saves current button as last pressed button for next frame.
 		lastPressedInput = pressedButton;
@@ -48,5 +93,25 @@ public class DanceMatActivity : Activity
 		}
 
 		return DanceMatInput.Up;
+	}
+
+	private Transform GetClosestGrape()
+	{
+		Transform closestGrape = fallingGrapes[0];
+		for (int i = 0; i < fallingGrapes.Count; i++)
+		{
+			if (Mathf.Abs(fallingGrapes[i].position.y - target.position.y) < Mathf.Abs(closestGrape.position.y - target.position.y))
+			{
+				closestGrape = fallingGrapes[i];
+			}
+		}
+
+		return closestGrape;
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(target.position, hitRadius);
 	}
 }
